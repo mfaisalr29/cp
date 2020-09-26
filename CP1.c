fd99bb64ae94c56a5a6ddfde9e139bf13bb51d2d
Faisal Ramadhan <34732068+mfaisalr29@users.noreply.github.com>
2020-09-26 12:20:34 +0700

    Add files via upload

---
+++ CP1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    //Hanya 1 perintah printf yang memberikan output 3 baris tulisan
+    printf("SELAMAT DATANG DI SEA CORP\n25 SEPTEMBER 2020\nCUACA CERAH");
+    return 0;
+}
